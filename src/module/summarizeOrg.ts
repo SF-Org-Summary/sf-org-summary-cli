0effc705ec97e18a8b50da3aa5c015386f432d05
Ruben <halmanrf@gmail.com>
2023-11-17 16:24:10 +0100

    init

--- src/module/summarizeOrg.ts
+++ src/module/summarizeOrg.ts
@@ -12,49 +12,59 @@ import { summary } from '../models/summary';
 import { GetFlowCoverage } from '../libs/GetFlowCoverage';
 
 const dataPoints = [
-    'Package2',
-    'RecommendationStrategy',
     'AIApplication',
     'ApexClass',
-    'ApexPage',
     'ApexExecutionOverlayAction',
+    'ApexPage',
     'ApexTrigger',
     'AuraDefinitionBundle',
     'AutoResponseRule',
+    'BusinessProcessDefinition',
     'BrandingSet',
     'CleanDataService',
+    'CleanRule',
     'CustomApplication',
     'CustomField',
     'CustomHttpHeader',
     'CspTrustedSite',
     'CustomObject',
     'CustomTab',
+    'DataIntegrationRecordPurchasePermission',
+    'DuplicateJobDefinition',
     'EmailTemplate',
-    'NamedCredential',
     'ExternalDataSource',
+    'FieldSet',
     'FlowDefinition',
-    'HomePageLayout',
-    'Group',
     'GlobalValueSet',
+    'Group',
+    'HomePageLayout',
     'InboundNetworkConnection',
     'LightningComponentBundle',
     'Layout',
     'LookupFilter',
+    'MatchingRule',
+    'MarketingAppExtension',
+    'NamedCredential',
+    'OpportunitySplitType',
     'OrgDomainLog',
     'OutboundNetworkConnection',
+    'Package2',
     'PathAssistant',
+    'PermissionSet',
     'PermissionSetGroup',
     'PermissionSet',
     'PlatformEventChannel',
-    'Profile',
     'PostTemplate',
+    'Profile',
     'QuickActionDefinition',
     'FieldSet',
     'RecordType',
+    'RecommendationStrategy',
     'RemoteProxy',
     'RestrictionRule',
-    'StaticResource',
+    'SchedulingRule',
     'Scontrol',
+    'StaticResource',
     'TransactionSecurityPolicy',
     'ValidationRule',
     'WebLink'
@@ -81,12 +91,11 @@ export function summarizeOrg(orgAlias?: string): summary {
     const instanceURL = JSON.parse(orgIdOutput).result.instanceUrl;
     const username = JSON.parse(orgIdOutput).result.username;
 
+    // RUN
     const testResultsCommand = `sfdx force:apex:test:run --target-org "${orgAlias}" --test-level RunLocalTests --code-coverage --result-format json > ./orgdata/testResults.json`;
     execSync(testResultsCommand, { encoding: 'utf8' });
-
     const testRunId = extractTestRunId('./orgdata/testResults.json');
     console.log('testRunId', testRunId);
-
     if (testRunId) {
         console.log(`Checking Status of Job "${testRunId}"...`);
         pollTestRunResult(testRunId, orgAlias)
@@ -95,8 +104,14 @@ export function summarizeOrg(orgAlias?: string): summary {
                     .then(testResult => {
                         getOrgWideApexCoverage(orgAlias)
                             .then(orgWideApexCoverage => {
-                                // Get flow coverage details
                                 const flowCoverage = getFlowCoverage(orgAlias);
+
+                                const apexClasses = queryApexClasses(orgAlias);
+                                const apexTriggers = queryApexTriggers(orgAlias);
+                                const auraComponents = queryAuraComponents(orgAlias);
+                                const lwcComponents = queryLWCComponents(orgAlias);
+                                const staticResources = queryStaticResources(orgAlias);
+                                // todo here: calculate lines of code in Apex Classes. In this make a distinction between the total, lines that represent comments, and the deduction of the comments from the total representing the actual lines of code.
 
                                 const queryResults: Record<string, unknown[]> = {};
                                 const errors = [];
@@ -104,7 +119,6 @@ export function summarizeOrg(orgAlias?: string): summary {
                                 for (const dataPoint of dataPoints) {
                                     try {
                                         const query = buildQuery(dataPoint);
-                                        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
                                         const result = queryMetadata(query, `./orgdata/${dataPoint}.csv`, orgAlias);
                                         queryResults[dataPoint] = result instanceof Array ? result : [];
                                     } catch (error) {
@@ -133,22 +147,17 @@ export function summarizeOrg(orgAlias?: string): summary {
                                         ApexTestMethodsFailed: testResult?.methodsFailed || 0,
                                         ApexOrgWideCoverage: orgWideApexCoverage || 0,
                                         FlowOrgWideCoverage: calculateFlowOrgWideCoverage(flowCoverage)
-                                    }
+                                    },
+                                    // todo here: Add the calculated scores of the like suggested in the structucre of the commented 'CodeLines' below.
+                                    // CodeLines: {
+                                    //     Apex: { Classes: { Total: number; Comments: number; Code: number }
+                                    // },
                                 };
 
                                 console.log('Summary:', summary);
                                 return summary;
-                            })
-                            .catch(error => {
-                                console.error('Error getting org-wide Apex coverage:', error.message);
                             });
-                    })
-                    .catch(error => {
-                        console.error('Error getting test run details:', error.message);
                     });
-            })
-            .catch(error => {
-                console.error('Error polling for test run results:', error.message);
             });
     }
 }
@@ -181,13 +190,13 @@ function getFlowCoverage(username: string): Record<string, number>[] {
 function calculateComponentSummary(queryResults: Record<string, unknown[]>): Record<string, unknown> {
     const componentSummary: Record<string, unknown> = {};
 
-    for (const dataPoint in queryResults) {
-        if (Object.prototype.hasOwnProperty.call(queryResults, dataPoint)) {
+    for (const dataPoint of dataPoints) {
+        if (queryResults[dataPoint]) {
             const results = queryResults[dataPoint];
             const resultLength = results.length;
 
             if (resultLength > 0) {
-                const lastRecord = results[0]; // Since we ordered by LastModifiedDate DESC, the first record has the latest modification
+                const lastRecord = results[0];
                 const lastModifiedDate = lastRecord.LastModifiedDate;
 
                 componentSummary[dataPoint] = {
@@ -197,6 +206,10 @@ function calculateComponentSummary(queryResults: Record<string, unknown[]>): Rec
             } else {
                 componentSummary[dataPoint] = { Total: 0 };
             }
+        } else {
+            // Handle the case where the query failed
+            console.error(`Query for '${dataPoint}' failed. Defaulting to 'N/A' in the summary.`);
+            componentSummary[dataPoint] = { Total: 'N/A' };
         }
     }
 
@@ -211,9 +224,9 @@ function buildQuery(dataPoint: string): string {
 function queryMetadata(query: string, outputCsv: string, orgAlias?: string) {
     let command;
     if (orgAlias) {
-        command = `sfdx data:query --query "${query}" --target-org "${orgAlias}" --result-format csv --use-tooling-api > ${outputCsv} `;
+        command = `sfdx data:query --query "${query}" --target-org "${orgAlias}" --result-format csv --use-tooling-api > ${outputCsv}`;
     } else {
-        command = `sfdx data:query --query "${query}" --result-format csv --use-tooling-api > ${outputCsv} `;
+        command = `sfdx data:query --query "${query}" --result-format csv --use-tooling-api > ${outputCsv}`;
     }
 
     try {
@@ -221,8 +234,14 @@ function queryMetadata(query: string, outputCsv: string, orgAlias?: string) {
         const csvData = fs.readFileSync(outputCsv, 'utf8');
         return parse(csvData, { columns: true });
     } catch (error) {
-        console.error(`Error executing command: ${command} `);
-        throw error;
+        // Handle errors related to unsupported sObject types
+        if (error.stderr.includes("sObject type") && error.stderr.includes("is not supported")) {
+            console.error(`Query for '${query}' is not supported. Defaulting to 'N/A' in the summary.`);
+            return [];
+        } else {
+            console.error(`Error executing command: ${command}`);
+            throw error;
+        }
     }
 }
 
@@ -296,15 +315,9 @@ async function getTestRunDetails(jobId: string, orgAlias?: string): Promise<{ ou
 
 async function getOrgWideApexCoverage(orgAlias?: string): Promise<number | null> {
     try {
-        // Formulate SOQL query to get org-wide Apex coverage
         const query = 'SELECT PercentCovered FROM ApexOrgWideCoverage';
         const results = await queryMetadata(query, './orgdata/orgWideApexCoverage.json', orgAlias);
-
-        // Calculate overall Apex coverage (average, sum, or as needed)
         const overallCoverage = results.reduce((sum, result) => sum + result.PercentCovered, 0) / results.length;
-
-        console.log(`Org-wide Apex Coverage: ${overallCoverage}%`);
-
         return overallCoverage;
     } catch (error) {
         console.error('Error getting org-wide Apex coverage:', error.message);
@@ -312,3 +325,27 @@ async function getOrgWideApexCoverage(orgAlias?: string): Promise<number | null>
     }
 }
 
+function queryApexClasses(orgAlias?: string): any[] {
+    const query = 'SELECT Id, CreatedDate, LastModifiedDate, CreatedBy.name, LastModifiedBy.name, APIVersion, Name, NamespacePrefix, LengthWithoutComments FROM ApexClass ORDER BY LastModifiedDate DESC';
+    return queryMetadata(query, './orgdata/ApexClasses.csv', orgAlias);
+}
+
+function queryApexTriggers(orgAlias?: string): any[] {
+    const query = 'SELECT Id, CreatedDate, LastModifiedDate, CreatedBy.name, LastModifiedBy.name, TableEnumOrId, Status, APIVersion, Name, NamespacePrefix, LengthWithoutComments FROM ApexTrigger ORDER BY LastModifiedDate DESC';
+    return queryMetadata(query, './orgdata/ApexTriggers.csv', orgAlias);
+}
+
+function queryAuraComponents(orgAlias?: string): any[] {
+    const query = 'SELECT ApiVersion, CreatedDate, CreatedBy.name, Id, LastModifiedDate, LastModifiedBy.name, Description, DeveloperName, ManageableState, MasterLabel, NamespacePrefix FROM AuraDefinitionBundle ORDER BY LastModifiedDate DESC';
+    return queryMetadata(query, './orgdata/AuraComponents.csv', orgAlias);
+}
+
+function queryLWCComponents(orgAlias?: string): any[] {
+    const query = 'SELECT CreatedDate, CreatedBy.name, Id, LastModifiedDate, LastModifiedBy.name, DeveloperName, ApiVersion, MasterLabel, NamespacePrefix, TargetConfigs, Description, IsExposed, IsExplicitImport FROM LightningComponentBundle ORDER BY LastModifiedDate DESC';
+    return queryMetadata(query, './orgdata/WebComponents.csv', orgAlias);
+}
+
+function queryStaticResources(orgAlias?: string): any[] {
+    const query = 'SELECT CacheControl, ContentType, CreatedBy.Name, CreatedDate, Id, LastModifiedBy.Name, LastModifiedDate, ManageableState, Name FROM StaticResource ORDER BY LastModifiedDate DESC';
+    return queryMetadata(query, './orgdata/StaticResources.csv', orgAlias);
+}
